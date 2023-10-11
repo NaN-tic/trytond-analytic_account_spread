c99de5e98366f75316cbda6ab5dba71b5e27d3d7
Albert Cervera i Areny <albert@nan-tic.com>
2023-10-11 12:13:36 +0200

    Pyflakes fixes

--- __init__.py
+++ __init__.py
@@ -1,15 +1,15 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .line import *
+from . import line
 
 
 def register():
     Pool.register(
-        SpreadAsk,
-        SpreadAskLine,
-        MoveLine,
+        line.SpreadAsk,
+        line.SpreadAskLine,
+        line.MoveLine,
         module='analytic_account_spread', type_='model')
     Pool.register(
-        SpreadWizard,
+        line.SpreadWizard,
         module='analytic_account_spread', type_='wizard')
